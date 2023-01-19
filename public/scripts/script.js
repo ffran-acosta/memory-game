15b029389b570214ba4480fedaf829782b0ed83b
ffran.acosta <francoaugustoacosta@gmail.com>
2023-01-19 17:32:59 -0300

    se muestran los primeros 2 numeros seleccionados

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,36 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('0')
-    button.addEventListener('click', () => {
-        window.alert('asd')
-    })
+    show()
 })
+
+// unordered pairs of numbers
+let numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
+numbers = numbers.sort(() => {return Math.random() - 0.5})
+console.log(numbers)
+
+//initial counter
+let reveal  = 0
+let firstPick = null
+let secondPick = null
+
+const btns = document.querySelectorAll('.btn')
+const mvoes = document.querySelector('.moves')
+
+const show = () => {
+    btns.forEach( btn => {
+        btn.addEventListener('click', () => {
+            reveal++
+
+            //first pick
+            if (reveal == 1) {
+                firstPick = btn
+                firstPick.innerHTML = numbers[btn.id]
+                firstPick.disabled = true
+            } else if (reveal == 2) {
+                //second pick
+                secondPick = btn
+                secondPick.innerHTML = numbers[btn.id]
+                secondPick.disabled = true
+            }
+        })
+    })
+}

ecdd5511e4a811f97ce1af9f741ba86fd4d90449
ffran.acosta <francoaugustoacosta@gmail.com>
2023-01-19 14:31:16 -0300

    memory game first commit

---
+++ public/scripts/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('0')
+    button.addEventListener('click', () => {
+        window.alert('asd')
+    })
+})
